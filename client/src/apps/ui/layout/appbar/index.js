c4cdb9379edbb4d14b57d005fa84264b497278e4
Yogesh Gupta <yogesh201996@gmail.com>
2019-03-20 02:11:54 +0530

    UI App-bar: WIP

--- client/src/apps/ui/layout/appbar/index.js
+++ client/src/apps/ui/layout/appbar/index.js
@@ -2,21 +2,42 @@ import React from 'react';
 import classnames from 'classnames';
 import {
     withStyles,
+    AppBar,
     Toolbar,
+    Hidden,
+    Typography,
 } from '@material-ui/core';
+
+import {
+    Menu,
+} from '@material-ui/icons';
+
 import styles from './styles';
 
-const AppBar = ({ className, classes, ...props }) => {
+const Appbar = ({ className, classes, ...props }) => {
     return (
-        <div 
+        <AppBar 
             className={classnames(classes.root, className)}
             {...props}
         >
             <Toolbar>
-                This is the toolbar
+                <Hidden smUp>
+                    <Menu color="primary" />    
+                </Hidden>
+                <div className={classes.linksContainer}>
+                    <Typography className={classes.link}>
+                        H<small>ome</small>
+                    </Typography>
+                    <Typography className={classes.link}>
+                        A<small>rticles</small>
+                    </Typography>
+                    <Typography className={classes.link}>
+                        A<small>bout</small> U<small>s</small> 
+                    </Typography>
+                </div>
             </Toolbar>
-        </div>
+        </AppBar>
     )
 }
 
-export default withStyles(styles)(AppBar);
+export default withStyles(styles)(Appbar);
--- client/src/apps/ui/layout/appbar/styles.js
+++ client/src/apps/ui/layout/appbar/styles.js
@@ -1,5 +1,53 @@
 export default theme => ({
     root: {
+        display: 'flex',
+        alignItems: 'center',
         width: '100%',
+        backgroundColor: theme.palette.background.appBar,
+        boxShadow: `0px 2px 4px -1px rgba(0, 0, 0, 0.1), 
+                    0px 4px 5px 0px rgba(0, 0, 0, 0.1)`,
+    },
+    linksContainer: {
+        display: 'flex',
+        flexGrow: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    link: {
+        position: 'relative',
+        marginLeft: 2 * theme.spacing.unit,
+        marginRight: 2 * theme.spacing.unit,
+        fontSize: '1.1em',
+        fontWeight: 300,
+        textTransform: 'uppercase',
+        cursor: 'pointer',
+        
+        '&:after': {
+            content: '""',
+            position: 'absolute',
+            left: '50%',
+            bottom: -theme.spacing.unit / 2,
+            width: '80%',
+            height: 2,
+            backgroundColor: theme.palette.primary.main,
+            transform: 'translate(-50%, 50%)',
+            opacity: 0,
+            transition: `transform 0.25s,
+                        opacity 0.25s`,
+        },
+
+        '&.active': {
+            cursor: 'initial',
+            
+            '&:after': {
+                transform: 'translate(-50%, 0%)',
+                opacity: 1,
+            },
+        },
+        
+        '&:hover:after': {
+            transform: 'translate(-50%, 0%)',
+            opacity: 1,
+        },
     },
 })
