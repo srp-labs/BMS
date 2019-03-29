8c53a16f691af649433d5de747d0d3fdb5e509a4
Jatin Goel <jatin.iiita@gmail.com>
2019-03-30 04:01:03 +0530

    Email Verification code function added

--- server/utility/views.py
+++ server/utility/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage
 
 # Create your views here.
+def send_otp():
+
+	code = '2345'
+	email = 'arijit.1790@gmail.com'
+	msg = 'Your Verification Code for React DJ is ' + '\'' + str(code) + '\'' + '. Please use this verification code for registration.\n\n' + 'NOTE: This is a system generated e-mail and please do not reply.\n\n' + 'Regards, \n' + 'Team React DJ'
+	email_msg = EmailMessage('React DJ: Verification Code',msg,to=[email])
+	try :
+		email_msg.send()
+		print("SUCCESS")
+
+	except Exception as e:
+		print(e)
+		print("FAILED")
+
+
+    
