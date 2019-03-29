a2341692d1c533d437b7fcb2e5c97e8fa7dfa1ac
Yogesh Gupta <yogesh201996@gmail.com>
2019-03-29 23:45:56 +0530

    Added sweet-alert to article-card

--- client/src/components/article-card/index.js
+++ client/src/components/article-card/index.js
@@ -15,10 +15,11 @@ import {
 } from '@material-ui/core';
 
 import {
-    Today,
     Timer,
 } from '@material-ui/icons';
 import styles from './styles';
+
+import SweetAlert from 'sweetalert2-react';
 
 import ChipsContainer from './chips-container';
 
@@ -72,17 +73,21 @@ const getChips = (data, theme) => {
     return chips;
 }
 
-const ArticleCard = ({ data, history, theme, classes, ...props }) => {
-    const isLoggedIn = Boolean(localStorage.getItem('username'));
-    const chips = getChips(data, theme);    
-    
-    const openArticle = () => {
+class ArticleCard extends React.Component {
+    state = {
+        showSweetAlert: false,
+        isLoggedIn: Boolean(localStorage.getItem('username')),
+    };
+
+    openArticle(data) {
         var win = window.open(data.url, '_blank');
         win.focus();
     }
-    
-    const markAsRead = (event) => {
-        if(isLoggedIn) {
+
+    markAsRead = (event) => {
+        event.preventDefault();
+
+        if (this.state.isLoggedIn) {
             // open rating sweet alert.
             console.log("Rating sweet alert.");
         }
@@ -90,63 +95,73 @@ const ArticleCard = ({ data, history, theme, classes, ...props }) => {
             // fire sweet alert with login redirect link.
             console.log("Login link sweet alert.");
         }
-    }
-    return (
-        <Grid item xs={12} md={6} lg={4}>
-            <Card className={classes.root}>
-                <CardActionArea className={classes.childMargins} onClick={openArticle}>
-                    <CardHeader 
-                        classes={{
-                            root: classes.cardHeaderRoot,
-                            content: classes.cardHeaderContent,
-                            subheader: classes.cardHeaderSubHeader,
-                        }}
-                        title={
-                            <Typography className={classes.articleTitle} color="secondary" title={data.title}>
-                                {data.title}
-                            </Typography>
-                        }
-                        subheader={new Date(data.publish_date).toDateString()} />
 
-                    <ChipsContainer chips={chips} />
+        this.setState({
+            showSweetAlert: true,
+        });
+    }
+
+    render() {
+        const { data, history, theme, classes, ...props } = this.props;
+
+        const chips = getChips(data, theme);
+
+        return (
+            <Grid item xs={12} md={6} lg={4}>
+                <Card className={classes.root}>
+                    <CardActionArea className={classes.childMargins} onClick={this.openArticle.bind(this, data)}>
+                        <CardHeader
+                            classes={{
+                                root: classes.cardHeaderRoot,
+                                content: classes.cardHeaderContent,
+                                subheader: classes.cardHeaderSubHeader,
+                            }}
+                            title={
+                                <Typography className={classes.articleTitle} color="secondary" title={data.title}>
+                                    {data.title}
+                                </Typography>
+                            }
+                            subheader={new Date(data.publish_date).toDateString()} />
+
+                        <ChipsContainer chips={chips} />
+
+                        <Divider className={classes.divider} />
+
+                        <div className={classes.cardContent}>
+                            <div className={classes.articleImageWrapper}>
+                                {
+                                    data.thumbnail && false ?
+                                        <img src={data.thumbnail} className={classes.articleImage} /> :
+                                        <img src={TestImage} className={classes.articleImage} />
+                                }
+                            </div>
+                        </div>
+                    </CardActionArea>
 
                     <Divider className={classes.divider} />
 
-                    <div className={classes.cardContent}>
-                        <div className={classes.articleImageWrapper}>
-                            {
-                                data.thumbnail && false ?
-                                <img src={data.thumbnail} className={classes.articleImage} />:
-                                <img src={TestImage} className={classes.articleImage} />
-                            }
-                        </div>
-                    </div>
-                </CardActionArea>
-
-                <Divider className={classes.divider} />
-                
-                <CardActions className={classes.metaInformation}>
-                    {/* <Typography className={classes.metaText}>
-                        <People />
-                        Jatin Goel
-                    </Typography> */}
-                    {/* <Typography className={classes.metaText}>
-                        <Today />
-                        {data.publish_date}
-                    </Typography> */}
-                    <Typography className={classes.metaText}>
-                        <Timer />
-                        {data.read_time} min
+                    <CardActions className={classes.metaInformation}>
+                        <Typography className={classes.metaText}>
+                            <Timer />
+                            {data.read_time} min
                     </Typography>
-                    <Button variant="outlined" color="secondary" onClick={markAsRead}>
-                        Mark as Read
+                        <Button variant="outlined" color="secondary" onClick={this.markAsRead}>
+                            Mark as Read
                     </Button>
-                </CardActions>
+                    </CardActions>
 
-                <div className={classes.bottomHighlight} />
-            </Card>
-        </Grid>
-    )
-}
+                    <div className={classes.bottomHighlight} />
+                </Card>
+
+                <SweetAlert
+                    show={this.state.showSweetAlert}
+                    title="Demo"
+                    text="SweetAlert in React"
+                    onConfirm={() => this.setState({ showSweetAlert: false })}
+                />
+            </Grid>
+        )
+    }
+} 
 
 export default withRouter(withTheme()(withStyles(styles)(ArticleCard)));
