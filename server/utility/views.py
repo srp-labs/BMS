b71c3a54321dbae624572d7f19a8cd7ac3df997f
Jatin Goel <jatin.iiita@gmail.com>
2019-03-30 05:24:04 +0530

    Gender and Region API Added

--- server/utility/views.py
+++ server/utility/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-from django.core.mail import EmailMessage
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.shortcuts import render
+
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth.models import Group,User
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView,RetrieveAPIView
+from utility.models import*
+from utility.serializers import*
+
+class GenderListView(APIView):
+
+	def get(self,request):
+
+		genders = Gender.objects.all()
+		serializer = GenderSerializer(genders,many=True)
+		return Response({'status': 'SUCCESS', 'list': serializer.data})
+
+
+class RegionListView(APIView):
+
+	def get(self,request):
+
+		regions = Region.objects.all()
+		serializer = RegionSerializer(regions,many=True)
+		return Response({'status': 'SUCCESS', 'list': serializer.data})
+
+
 def send_otp():
 
 	code = '2345'
