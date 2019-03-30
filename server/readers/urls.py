400e5b2be555fbf45ec12d5585c91a38aadb05e9
Jatin Goel <jatin.iiita@gmail.com>
2019-03-30 05:38:08 +0530

    Reader Registration Post Request

--- server/readers/urls.py
+++ server/readers/urls.py
@@ -1,1 +1,10 @@
-urls.py
+from django.conf.urls import url
+from django.urls import path, include
+from utility.views import*
+
+urlpatterns = [
+
+	
+    url(r'^api/registration/$',RegistrationListView.as_view(),name='view-all'),
+
+]
