4277b531bbb5454f76d4b89f3c3a3767e5c0d6cc
Maharshi Roy <strategist.roy@gmail.com>
2019-04-03 20:23:48 +0530

    Produced Bundle File

--- server/server/settings.py
+++ server/server/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
